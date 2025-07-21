6fd56b93f9e1ada31c64daa1fa16ca2d0a103386
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-07-21 17:08:53 +0300

    add scripts for local deploing

--- backend/star_burger/settings.py
+++ backend/star_burger/settings.py
@@ -11,7 +11,7 @@ STATIC_ROOT = (BASE_DIR / 'staticfiles').resolve()
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost', '82.202.129.246'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
